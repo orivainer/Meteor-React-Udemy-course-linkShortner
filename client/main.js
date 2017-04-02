3e969f89aa11692fea309da8a0b8a3ddcfbceae7
Ori <hello.ori@gmail.com>
2017-04-02 11:55:28 +0300

    Boilerplate

---
+++ client/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <h1>Hello world!</h1>
+    );
+}
+
+Meteor.startup(() => {
+    ReactDOM.render(<App />, document.getElementById('AppContainer'));
+});
