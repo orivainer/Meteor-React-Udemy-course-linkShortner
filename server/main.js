53e9cd9b3e2e9c048f0978801d9a8d2d60779817
Ori <hello.ori@gmail.com>
2017-04-05 14:29:46 +0300

    Finished section 3 of the course

--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import { Links } from '../imports/collections/links';
+import { WebApp } from 'meteor/webapp';
+import ConnectRoute from 'connect-route';
+
 
 Meteor.startup(() => {
   // code to run on server at startup
@@ -9,3 +12,22 @@ Meteor.startup(() => {
     });
   });
 });
+
+function onRoute(req, res, next) {
+  const link = Links.findOne({ token: req.params.token });
+  //If a token was found - redirection is needed
+  if (link) {
+    Links.update(link, { $inc: { clicks: 1 } }); // Increase the clicks fields by 1
+    res.writeHead(307, { 'Location': link.url });
+    res.end();
+  } else { //If a token wasn't found, forward to our normal app
+    next();
+  }
+
+}
+
+const redirectMiddleware = ConnectRoute(function (router) {
+  router.get('/:token', onRoute);
+});
+
+WebApp.connectHandlers.use(redirectMiddleware);
